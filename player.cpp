a02712732cf58141aae2f12c822d43ee45754833
skaliak <info@skaliak.com>
2014-03-09 16:38:11 -0700

    added player member stubs

--- player.cpp
+++ player.cpp
@@ -1,2 +1,27 @@
 
-#include "Player.h"
+#include "player.h"
+
+Plater::Player()
+{
+	//?
+}
+
+//this should get an array of room pointers or something?
+void showExits()
+{
+
+}
+
+//gets the current room description and list of exits
+void lookAround()
+{
+
+}
+
+//moves the player in this direction
+void move(Direction dir)
+{
+	//need to check if it's possible to move in that direction, then
+	//lastRoom = currentRoom and
+	//currentRoom = currentRoom->direction
+}
--- player.h
+++ player.h
@@ -1,6 +1,7 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <string>
 #include "entity.h"
 #include "room.h"
 #include "map.h"
@@ -13,6 +14,7 @@ public:
 	void lookAround(); //gets the current room description and list of exits
 	void move(Direction dir);  //moves the player in this direction
 private:
+	std::string name;
 	Room *currentRoom;
 	Room *lastRoom;
 	Map *map;	
